dabe2fde8089a67124134425fc0e443ec4244be8
wide-aze <wide-aze@student.42.fr>
2015-04-04 12:21:38 +0200

    parser

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -45,6 +45,81 @@ float                   getStrHeight(sf::Text const &ref)
 }
 }
 
+#include <cstdlib>
+#include <string>
+
+void					parse_input(int ac, char **av, 
+std::vector<ft::IMonitorModule*> &modules,
+std::list<ft::IMonitorDisplay*> &displays)
+{
+	if (ac <= 1)
+		return ;
+	std::string					type = av[1];
+	bool						n = false;
+	bool						s = false;
+	int							nb = atoi(&av[1][1]);
+	std::vector<std::string> 	args(av + 2, av + ac);
+
+	if (*av[1]++ != '-')
+	{
+		std::cout << "Bad graphical options" << std::endl;	
+		return ;
+	}
+	while (*av[1] >= '0' && *av[1] <= '9')
+		av[1]++;
+	while (*av[1] == 's' || *av[1] == 'n')
+	{
+		if (*av[1] == 's')
+			s = true;
+		else if (*av[1] == 'n')
+			n = true;
+		av[1]++;
+	}
+	if (*av[1] != '\0')
+	{
+		std::cout << "Bad graphical options" << std::endl;	
+		return ;
+	}
+	if (s)//debug
+		std::cout << "s" << std::endl;
+	if (n)
+		std::cout << "n" << std::endl;//fin debug
+	if (s || n)
+	{
+		while (--nb >= 0)
+		{
+			std::cout << "SFML: Loading ..." << std::endl;
+			ftsf::Arial.loadFromFile("srcs/ft_sfml/Liberation.ttf"); //verif
+			if (s)
+			{
+				displays.push_back(new ftsf::Window(modules,
+					ftsf::Window::calculateWindowSize(modules)));
+			}
+			if (n) 
+			{
+				;
+			}
+			std::cout << "SFML: Finished Loading ..." << std::endl;	
+		}
+		for (std::vector<std::string>::iterator it = args.begin(); it != args.end(); it++)
+		{
+			std::transform(it->begin(), it->end(), it->begin(), ::tolower);
+			if (*it == "hostname")
+				modules.push_back(new ft::TimeModule("Time"));
+			else if (*it == "osinfo")
+				modules.push_back(new ft::TimeModule("Time"));
+			else if (*it == "date")
+				modules.push_back(new ft::TimeModule("Time"));
+			else if (*it == "cpu")
+				modules.push_back(new ft::TimeModule("Time"));
+			else if (*it == "ram")
+				modules.push_back(new ft::TimeModule("Time"));
+			else if (*it == "network")
+				modules.push_back(new ft::TimeModule("Time"));
+		}
+	}
+}
+
 #include <unistd.h>
 
 int						main(int ac, char *av[])
@@ -56,27 +131,7 @@ int						main(int ac, char *av[])
 	
 	try
 	{
-		//lire les argv pour fill modules
-		(void)ac;
-		(void)av;
-		modules.push_back(new ft::TimeModule("Time1"));
-		modules.push_back(new ft::TimeModule("Time2"));
-		//lire les argv pour fill modules
-
-		
-		if (1) //si il faut lancer la sfml
-		{
-			std::cout << "SFML: Loading ..." << std::endl;
-			ftsf::Arial.loadFromFile("srcs/ft_sfml/Liberation.ttf"); //verif
-			displays.push_back(new ftsf::Window(
-								   modules,
-								   ftsf::Window::calculateWindowSize(modules)));
-			std::cout << "SFML: Finished Loading ..." << std::endl;	
-		}
-		if (0)
-		{
-			//si il faut lancer la ncurse
-		}
+		parse_input(ac, av, modules, displays);
 	}
 	catch (...)
 	{
